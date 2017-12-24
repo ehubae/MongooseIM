246c96e52b368a0d4a985c11904ccc8f1bb4492c
jaspreet-android <jaspreet.android@gmail.com>
2017-12-24 17:53:33 +0530

    added time in sent receipt

--- apps/ejabberd/src/jlib.erl
+++ apps/ejabberd/src/jlib.erl
@@ -186,7 +186,8 @@ make_sent_reply_attrs(Attrs) ->
            end,
   Attrs5 = lists:keydelete(<<"type">>, 1, Attrs4),
   Attrs6 = [{<<"type">>, <<"chat">>} | Attrs5],
-  Attrs6.
+  Attrs7 =[{<<"time">>,ejabberd_binary:string_to_binary(os:timestamp())}|Attrs6],
+  Attrs7.
 
 -spec make_config_change_message(binary()) -> xmlel().
 make_config_change_message(Status) ->
